74e3cc44e687f8013287709ce318c693e3537168
OleksejG <78223921+OleksejG@users.noreply.github.com>
2021-02-08 22:55:45 +0300

    v0.1

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() 
+	{$('.heder__burger').click(function(event) {
+		$('.heder__burger,.heder__menu').toggleClass('active');
+	});
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# TestWork.github.io
+ 
